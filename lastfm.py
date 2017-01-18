c2c34e60a2adf77a1d6ec7ca6d8aad44f990d2f5
Noah Huber-Feely <nhuberfeely@gmail.com>
2017-01-18 17:07:33 -0600

    Improve authorization function

--- lastfm.py
+++ lastfm.py
@@ -1,12 +1,19 @@
 # Last.fm Syncing Tool
 import os
 import requests
+import md5
 
 api_head = 'http://ws.audioscrobbler.com/2.0/'
+secret = 'the_secret_sauce'
 
 def authorize(user_token):
-    requestHash = None # TODO: Generate MD5 hash
-    apiResp = requests.post(api_head, {'method': 'auth.getSession', 'apiKey': os.environ['LAST_FM_API'], 'apiSig': requestHash})
+    stringToHash = 'api_key' + os.environ['LAST_FM_API'] + 'method' + 'auth.getSession' + 'token' + user_token + secret
+    stringToHash = stringToHash.encode('utf8')
+    print stringToHash
+    requestHash = md5.new(stringToHash).digest()
+    params = {'api_key': os.environ['LAST_FM_API'], 'method': 'auth.getSession', 'token': user_token, 'api_sig': requestHash}
+    print params
+    apiResp = requests.post(api_head, params)
     return apiResp.text
 
 def scrobble(song_name, artist_name):
