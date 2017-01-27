f7f3a94d28ad1366d442a3faddf8aa4fc305b583
Noah Huber-Feely <nhuberfeely@gmail.com>
2017-01-27 09:14:41 -0600

    Separate api root and add user_weekly_tracks method

--- extras.py
+++ extras.py
@@ -3,6 +3,12 @@
 import requests
 import os
 
+api_root = 'http://ws.audioscrobbler.com/2.0/'
+
 def user_tracks(user_name):
-    resp = requests.get('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + user_name + '&api_key=' + os.environ['LAST_FM_API'] + '&format=json')
+    resp = requests.get(api_root + '?method=user.getrecenttracks&user=' + user_name + '&api_key=' + os.environ['LAST_FM_API'] + '&format=json')
+    print resp.text
+
+def user_weekly_tracks(user_name):
+    resp = requests.get(api_root + '?method=user.getweeklytrackchart&user=' + user_name + '&api_key=' + os.environ['LAST_FM_API'] + '&format=json')
     print resp.text
