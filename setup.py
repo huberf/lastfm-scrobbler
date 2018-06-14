92630bbf7c988ef928517348f476f2336d4d0e2a
Noah Huber-Feely <nhuberfeely@gmail.com>
2018-06-14 19:24:12 -0400

    Upgrade version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'lastpy',
   packages = ['lastpy'], # this must be the same as the name above
-  version = '1.0.4',
+  version = '1.0.5',
   description = 'A straightforward utility for scrobbling tracks to last.fm',
   author = 'Noah Huber-Feely',
   author_email = 'nhuberfeely@gmail.com',
   url = 'https://github.com/huberf/lastfm-scrobbler', # use the URL to the github repo
-  download_url = 'https://github.com/huberf/lastfm-scrobbler/archive/1.0.4.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/huberf/lastfm-scrobbler/archive/1.0.5.tar.gz', # I'll explain this in a second
   keywords = ['lastfm'], # arbitrary keywords
   classifiers = [],
 )
